0278a778e49f7c3b5811a4221b2ed6c2ea561da0
Liliana Leyva <ingeleyva@yahoo.com>
2020-11-30 01:34:54 -0600

    mvp

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const StyledH2 = styled.h2`
+    border: solid green 2px;
+    padding: 8px 8px;
+    text-align: center;
+    color: #F4F2C9;
+    background-color: #265999 ;
+`;
+function Character(props) {
+    return (
+        <StyledDiv>
+        <div>{props.characters.map(char => {
+            return <StyledH2 key={char.name}>{char.name} {char.birth_year}</StyledH2>
+        })}
+        </div>
+        </StyledDiv>
+    )
+}
+
+export default Character;
